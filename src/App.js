ff9ae64246d69b966f0c9b0fa5d651bb6ce66655
lhunter50 <luxstme@gmail.com>
2022-03-30 22:25:56 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
+import { Layout, Menu, Breadcrumb } from 'antd';
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+        <div className='logo' mode='horizontal' />
+          <Menu mode='horizontal'>
+            <Menu.Item key='1'>Nav 1</Menu.Item>
+            <Menu.Item key='2'>Nav 2</Menu.Item>
+            <Menu.Item key='3'>Nav 3</Menu.Item>
+          </Menu>  
+        </Header>
+        <Breadcrumb style={{padding: '16px 0'}}></Breadcrumb>
+        <Content className='site-layout' style={{ padding: '0 50px', marginTop: 64, backGroundColor: 'grey'}}>
+          <div className='site-layout-background' style={{ padding: 24, minHeight: 380, backgroundColor: '#707070'}}>
+            Content  
+          </div>
+        </Content>
+        <Footer style={{ textAlign:'center'}}>Lachlan Hunter ©2020</Footer>
+    </Layout>
   );
 }
 
